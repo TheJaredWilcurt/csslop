90c5660a0ca9f97afc405d2679b53afdff25f9ee
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2026-06-21 11:39:04 -0400

    Progress

--- tests/realworld.test.js
+++ tests/realworld.test.js
@@ -2,7 +2,11 @@
  * @file Runs all the real world CSS files through the minify as a test.
  */
 
-import { writeFileSync } from 'node:fs';
+import {
+  existsSync,
+  readFileSync,
+  writeFileSync
+} from 'node:fs';
 import { join } from 'node:path';
 
 import getRealWorldCSS from 'real-world-css-libraries';
@@ -10,8 +14,24 @@ import getRealWorldCSS from 'real-world-css-libraries';
 import { minifyCSS } from '../index.js';
 
 const __dirname = import.meta.dirname;
+const reportPath = join(__dirname, '..', 'realWorldResults.json');
+const minifiedPath = join(__dirname, 'minified');
+const existingReport = JSON.parse(readFileSync(reportPath));
+const librariesReport = existingReport.libraries;
+const totalsReport = existingReport.totals;
 
+let skipped = false;
 let libraries = getRealWorldCSS();
+// Combine existing report data.
+libraries = libraries.map((library) => {
+  const report = librariesReport.find((reportLibrary) => {
+    return reportLibrary.fileName === library.fileName;
+  });
+  return {
+    ...library,
+    ...(report || {})
+  };
+});
 
 let longestName = 0;
 libraries.forEach((library) => {
@@ -23,30 +43,42 @@ libraries.forEach((library) => {
 
 for (let i = 0; i < libraries.length; i++) {
   const library = libraries[i];
-  const start = Date.now();
-  console.log('');
-  console.log(
-    library.name.padEnd(longestName) +
-    ' - ' +
-    library.source.length +
-    ' - ' +
-    (i + 1) +
-    '/' +
-    libraries.length
-  );
-  const output = minifyCSS(library.source);
-  const duration = Date.now() - start;
-  const difference = library.source.length - output.length;
+  let output;
+  let duration;
+  const outputFile = join(minifiedPath, library.fileName);
+  const fileAlreadyExists = existsSync(outputFile);
+  if (fileAlreadyExists) {
+    output = String(readFileSync(outputFile)).trim();
+    skipped = true;
+    duration = library.duration;
+  } else {
+    console.log('');
+    console.log(
+      library.name.padEnd(longestName) +
+      ' - ' +
+      library.source.length +
+      ' - ' +
+      (i + 1) +
+      '/' +
+      libraries.length
+    );
+    const start = Date.now();
+    output = minifyCSS(library.source);
+    duration = Date.now() - start;
+  }
   const percent = Math.round((output.length / library.source.length) * 100);
-  console.log(
-    ((duration / 1000) + 's').padEnd(longestName) +
-    ' - ' +
-    output.length +
-    ' - ' +
-    difference +
-    ' - ' +
-    percent + '%'
-  );
+  if (!fileAlreadyExists) {
+    const difference = library.source.length - output.length;
+    console.log(
+      ((duration / 1000) + 's').padEnd(longestName) +
+      ' - ' +
+      output.length +
+      ' - ' +
+      difference +
+      ' - ' +
+      percent + '%'
+    );
+  }
   library.duration = duration;
   library.inputSize = library.source.length;
   library.outputSize = output.length;
@@ -94,13 +126,12 @@ console.log('TOTAL INPUT: ' + (Math.round(totalInput / 1024 / 1024 * 100) / 100)
 console.log('TOTAL OUTPUT: ' + (Math.round(totalOutput / 1024 / 1024 * 100) / 100) + 'MB');
 
 for (const library of libraries) {
-  writeFileSync(join(__dirname, 'minified', library.fileName), library.output + '\n');
+  writeFileSync(join(minifiedPath, library.fileName), library.output + '\n');
   delete library.fileName;
   delete library.output;
   delete library.license;
 }
 
-const reportPath = join(__dirname, '..', 'realWorldResults.json');
 const report = JSON.stringify({
   totals: {
     inputSize: totalInput,
