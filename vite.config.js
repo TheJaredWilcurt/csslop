5a39507407270b9536f8a8dc15b5fd5791971b53
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2026-06-22 14:42:59 -0400

    Site

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+import { resolve } from 'node:path';
+
 import { defineConfig } from 'vite';
+
+const __dirname = import.meta.dirname;
 
 const config = defineConfig({
   base: '/csslop',
   build: {
+    outDir: resolve(__dirname, 'site'),
     sourcemap: true
   }
 });
